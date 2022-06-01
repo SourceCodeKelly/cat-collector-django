91c0077d42a85403ce75e2a50b5938b2765ae10d
Anthony <96215255+SourceCodeKelly@users.noreply.github.com>
2022-06-01 14:36:51 -0400

    Base page has Home and About pages extended

---
+++ main_app/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.template import Template
+from django.views import View #
+from django.http import HttpResponse
+from django.views.generic.base import TemplateView
+
+# Create your views here.
+class Home(TemplateView):
+    template_name = 'home.html'
+    
+#...
+class About(TemplateView):
+    template_name = 'about.html'

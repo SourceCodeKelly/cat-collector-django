8b0cc99799b4c0cf6485cdd9c752de528c277a29
Anthony <96215255+SourceCodeKelly@users.noreply.github.com>
2022-06-01 15:41:08 -0400

    Static pages served

--- cat_collector_project/settings.py
+++ cat_collector_project/settings.py
@@ -30,8 +30,9 @@ SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
+STATIC_URL = True
 ALLOWED_HOSTS = []
+
 
 
 # Application definition
@@ -123,9 +124,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'main_app/static')]

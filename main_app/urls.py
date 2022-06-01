e284d1b43ef4b701b1a630c0e2792e60983369a1
Anthony <96215255+SourceCodeKelly@users.noreply.github.com>
2022-06-01 17:02:41 -0400

    Cat List created and showing

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.Home.as_view(), name="home"),
     path('about/', views.About.as_view(), name='about'),
+    path('cats/', views.CatList.as_view(), name='cat_list')
 ]
